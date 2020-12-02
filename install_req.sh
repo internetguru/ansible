6991c4bc09a7a0a67bc1722a3f647ca6353f1504
petrzpav <pavel.petrzela@internetguru.cz>
2020-12-02 14:53:24 +0100

    fix perms

--- install_req.sh
+++ install_req.sh
@@ -2,9 +2,14 @@
 
 # create global cache folder
 mkdir -p /tmp/facts_cache
+chmod 777 /tmp/facts_cache
+
 
 # install ansible requirements
 ansible-galaxy collection install community.general
 ansible-galaxy install -r requirements.ubuntu.yml
 ansible-galaxy install -r requirements.ubuntu-dev.yml
+
+# make cache accessible for other users
+chmod -R 755 /usr/share/ansible
 
