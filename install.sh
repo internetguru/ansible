9a83eb9b009326a3ff3895e5872f23d30b6bfe77
petrzpav <pavel.petrzela@internetguru.cz>
2022-03-21 18:52:17 +0100

    Consolidate variables, fix shellcheck warnings

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@ trash=".zsh_history .zshrc.local .zshrc zshrc vimrc bashcfg omgf butt .ansible"
 
 exception() {
   echo "[EXCEPTION] ${1:-Unknown exception} in $(basename "${0}")" >&2
+  # shellcheck disable=SC2086
   exit ${2:-1}
 }
 EXC=$(declare -f exception)
 run_playbooks() {
-  echo "Installing $1 [$2] as $(whoami)"
-  ansible-playbook --connection=local --inventory 127.0.0.1, --tags "$2" $1 \
-    || exception "Failed to install $1 [$2] as $(whoami)"
+  echo "Installing ${1} [${2}] as $(whoami)"
+  ansible-playbook --connection=local --inventory 127.0.0.1, --tags "${2}" "${1}" \
+    || exception "Failed to install ${1} [${2}] as $(whoami)"
 }
 RUN=$(declare -f run_playbooks)
 
@@ -32,31 +33,32 @@ chmod -R 755 /usr/share/ansible \
   || exception "Failed to set permissions to /usr/share/ansible"
 
 # install global tasks
-bash -c "$EXC; $RUN; run_playbooks fresh_env.yml global" \
+bash -c "${EXC}; ${RUN}; run_playbooks fresh_env.yml global" \
   || exit 1
-bash -c "$EXC; $RUN; run_playbooks ubuntu.yml global" \
+bash -c "${EXC}; ${RUN}; run_playbooks ubuntu.yml global" \
   || exit 1
 
 # install user tasks for all users
 for user in $(getent passwd {1000..2000} | cut -d: -f1); do
   # install user files and settings
-  sudo -H -u "$user" bash -c "$EXC; $RUN; run_playbooks fresh_env.yml user" \
+  sudo -H -u "${user}" bash -c "${EXC}; ${RUN}; run_playbooks fresh_env.yml user" \
     || exit 1
-  sudo -H -u "$user" bash -c "$EXC; $RUN; run_playbooks ubuntu.yml user" \
+  sudo -H -u "${user}" bash -c "${EXC}; ${RUN}; run_playbooks ubuntu.yml user" \
     || exit 1
   # set default shell
-  usermod -s /usr/bin/bash "$user" \
+  usermod -s /usr/bin/bash "${user}" \
     || exit 1
   # remove previous mess
-  for f in $trash; do
-    rm -rf "/home/$user/$f"
+  for file in $trash; do
+    # shellcheck disable=SC2115
+    rm -rf "/home/${user}/${file}"
   done
 done
 
 # install ubuntu-dev for current user
-bash -c "$EXC; $RUN; run_playbooks ubuntu-dev.yml global" \
+bash -c "${EXC}; ${RUN}; run_playbooks ubuntu-dev.yml global" \
   || exit 1
-sudo -H -u "$(logname)" bash -c "$EXC; $RUN; run_playbooks ubuntu-dev.yml user" \
+sudo -H -u "$(logname)" bash -c "${EXC}; ${RUN}; run_playbooks ubuntu-dev.yml user" \
   || exit 1
 
 # uninstall previous apps
@@ -64,6 +66,6 @@ ansible-playbook --connection=local --inventory 127.0.0.1, clear_env.yml \
   || exception "Failed to clear enviroment"
 
 # remove previous mess for root
-for f in $trash; do
-  rm -rf "/root/$f"
+for file in ${trash}; do
+  rm -rf "/root/${file}"
 done
