863979df1482092713fe7678e8a4c93ef536074f
petrzpav <pavel.petrzela@internetguru.cz>
2022-03-22 07:56:19 +0100

    Install only passed configuration

--- install.sh
+++ install.sh
@@ -18,14 +18,34 @@ main() {
   declare -r TRASH_FILES=".zsh_history .zshrc.local .zshrc zshrc vimrc bashcfg omgf butt .ansible"
   # shellcheck disable=SC2155
   declare -r DIR="$(dirname "$0")"
-  # force config location
-  export ANSIBLE_CONFIG="${DIR}/ansible.cfg"
-
+  declare FORCE=0
+  ## option preprocessing
+  if ! LINE=$(getopt -n "$0" -o f -l force -- "$@"); then
+    exit 1
+  fi
+  eval set -- "$LINE"
+  ## load user options
+  while [ $# -gt 0 ]; do
+    case $1 in
+      -f|--force) FORCE=1; shift ;;
+      --) shift; break ;;
+      *-) echo "$0: Unrecognized option '$1'" >&2; exit 2 ;;
+       *) break ;;
+    esac
+  done
+  [[ -z "${1}" ]] \
+    && exception "Missing config path(s)"
+  for config in "$@"; do
+    [[ ! -f "${config}" ]] \
+      && exception "Configuration file ${config} not found"
+  done
   [[ $(id -u) != 0 ]] \
     && exception "Script must be run as root"
   # create global cache folder
   mkdir -p /tmp/facts_cache
   chmod 777 /tmp/facts_cache
+  # force config location
+  export ANSIBLE_CONFIG="${DIR}/ansible.cfg"
   # install ansible requirements
   ansible-galaxy collection install community.general \
     || exit 1
@@ -34,17 +54,17 @@ main() {
   chmod -R 755 /usr/share/ansible \
     || exception "Failed to set permissions to /usr/share/ansible"
   # install global tasks
-  bash -c "${EXC_DEF}; ${RUN_DEF}; run_playbooks ${DIR}/fresh_env.yml global" \
-    || exit 1
-  bash -c "${EXC_DEF}; ${RUN_DEF}; run_playbooks ${DIR}/ubuntu.yml global" \
-    || exit 1
+  for config in "$@"; do
+    bash -c "${EXC_DEF}; ${RUN_DEF}; run_playbooks ${config} global" \
+      || exit 1
+  done
   # install user tasks for all users
   for user in $(getent passwd {1000..2000} | cut -d: -f1); do
     # install user files and settings
-    sudo -H -u "${user}" bash -c "${EXC_DEF}; ${RUN_DEF}; run_playbooks ${DIR}/fresh_env.yml user" \
-      || exit 1
-    sudo -H -u "${user}" bash -c "${EXC_DEF}; ${RUN_DEF}; run_playbooks ${DIR}/ubuntu.yml user" \
-      || exit 1
+    for config in "$@"; do
+      sudo -H -u "${user}" bash -c "${EXC_DEF}; ${RUN_DEF}; run_playbooks ${config} user" \
+        || exit 1
+    done
     # set default shell
     usermod -s /usr/bin/bash "${user}" \
       || exit 1
