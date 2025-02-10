bc15c4717e098cfa44b56d60f4c10ceef236b66c
petrzpav <pavel.petrzela@internetguru.cz>
2025-02-10 07:28:18 +0100

    Add ubuntu version check

--- install.sh
+++ install.sh
@@ -44,6 +44,9 @@ main() {
   done
   [[ $(id -u) != 0 ]] \
     && exception "Script must be run as root"
+  # check for supported ubuntu version
+  [[ $(lsb_release -cs) != "noble" ]] \
+    || exception "Unsupported Ubuntu version, please use Ubuntu 24.04"
   # force config location for ansible
   export ANSIBLE_CONFIG="${WORKDIR}/ansible.cfg"
   eval "$(tail --lines=+2 "${ANSIBLE_CONFIG}")"
